46cfe17697de6e0c5fe77dfd740e7e26dc723639
Admin <Badr.Ouaddah.07@gmail.com>
2023-07-02 18:25:19 +0100

    Add some minor changes in the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
-import { Container, Row, Col } from 'react-bootstrap';
-import { PizzaCard } from './components/PizzaCard/PizzaCard';
-import  { Confirmation }  from './components/Confirmation/Confirmation';
-import pizzas from './data.json';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import { Container, Row, Col } from "react-bootstrap";
+import { PizzaCard } from "./components/PizzaCard/PizzaCard";
+import { Confirmation } from "./components/Confirmation/Confirmation";
+import pizzas from "./data.json";
 
 function App() {
   return (
-        <Container>
-          <Row>
-          <Confirmation/>
-          </Row>
-        </Container>
+    <div>
+      <Confirmation />
+      <Container>
+        <Row>
+          {pizzas.map(data =>{
+            <div>
+              {data.id}
+              {data.name}
+              {data.image}
+              {data.price}
+              {data.desc}
+            </div>
+          })}
+        </Row>
+      </Container>
+    </div>
   );
 }
 
--- src/components/Confirmation/Confirmation.jsx
+++ src/components/Confirmation/Confirmation.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Toast } from 'react-bootstrap';
+import './Confirmation.scss';
 
 export function Confirmation({ toggle }) {
   return (
+    <div className='black-color'>
     <Toast onClose={() => toggle(false)}>
       <Toast.Header>
         <strong className="mr-auto">Your Order Is In The Oven</strong>
@@ -12,5 +14,6 @@ export function Confirmation({ toggle }) {
         Your delicious pizza will be with you in 30 minutes!
       </Toast.Body>
     </Toast>
+    </div>
   );
 }
