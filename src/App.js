b51731f0a0fe6570f42d72a926817f61563e11db
Admin <Badr.Ouaddah.07@gmail.com>
2023-07-02 15:57:38 +0100

    Add Toasts react-bootstrap in Confirmation component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 import { Container, Row, Col } from 'react-bootstrap';
+import { PizzaCard } from './components/PizzaCard/PizzaCard';
+import  { Confirmation }  from './components/Confirmation/Confirmation';
 import pizzas from './data.json';
+
 
 function App() {
   return (
-    <div>
-        PIZZA
-    </div>
+        <Container>
+          <Row>
+          <Confirmation/>
+          </Row>
+        </Container>
   );
 }
 
